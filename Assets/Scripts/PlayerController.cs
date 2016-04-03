68992e8992f2e3a61d41aee4a161edc77eafa0a3
pant2002 <aniketpant@Anikets-MacBook-Air.local>
2016-04-02 19:35:57 -0500

    Added project

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+[RequireComponent(typeof(ConfigurableJoint))]
+public class PlayerController : MonoBehaviour {
+
+	private ConfigurableJoint joint;
+
+	public float speed = 5f;
+
+	public float lookSensitivity = 3f;
+
+	[SerializeField]
+	private float thrusterForce = 1000f;
+
+	private PlayerMotor motor;
+
+	void Start ()
+	{
+
+		motor = GetComponent<PlayerMotor>();
+		joint = GetComponent<ConfigurableJoint> ();
+
+	}
+
+	void Update ()
+	{
+		float _zMov = Input.GetAxisRaw ("Vertical");
+		float _xMov = Input.GetAxisRaw ("Horizontal"); 
+
+		Vector3 _movHorizontal = transform.right * _xMov;
+		Vector3 _movVertical = transform.forward * _zMov;
+        //final calculation
+		Vector3 _velocity = (_movHorizontal + _movVertical).normalized * speed;
+        
+       //apply calculations for motion
+       motor.Move(_velocity);
+
+		//Sideways camera rotation
+		float _yRot = Input.GetAxisRaw("Mouse X");
+        
+		Vector3 _rotation = new Vector3 (0f, _yRot, 0f) * lookSensitivity;
+
+		motor.Rotate (_rotation);
+
+
+		//UP-DOWN camera
+		float _xRot = Input.GetAxisRaw("Mouse Y");
+
+		Vector3 _cameraRotation = new Vector3 (_xRot, 0f , 0f) * lookSensitivity;
+
+		motor.RotateCamera (_cameraRotation);
+
+		//Apply thruster force:
+
+		//Calculate force:
+		Vector3 _thrusterForce = Vector3.zero;
+
+		if (Input.GetButton ("Jump")) {
+			_thrusterForce = Vector3.up * thrusterForce;
+		}
+
+		motor.ApplyThruster (_thrusterForce);
+	}
+}
---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour {
+
+	public Camera cam;
+	private Vector3 velocity = Vector3.zero;
+    private Rigidbody rb;    
+	private Vector3 rotation = Vector3.zero;
+	private Vector3 cameraRotation = Vector3.zero;
+	private Vector3 thrusterForce = Vector3.zero;
+   
+    void Start()
+    {
+       rb = GetComponent<Rigidbody>();  
+    }
+
+    public void Move(Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+
+	public void Rotate(Vector3 _rotation)
+	{
+		rotation = _rotation;
+	}
+    
+	public void RotateCamera(Vector3 _cameraRotation)
+	{
+		cameraRotation = _cameraRotation;
+	}
+
+	public void ApplyThruster (Vector3 _thrusterForce){
+		thrusterForce = _thrusterForce;
+	}
+
+
+	void FixedUpdate()
+    {
+        PerformMovement();
+		PerformRotation();
+    }
+    
+    void PerformMovement()
+	{
+        if (velocity != Vector3.zero)
+        {
+             rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        }
+
+		if (thrusterForce != Vector3.zero) {
+			rb.AddForce (thrusterForce * Time.fixedDeltaTime, ForceMode.Acceleration);
+		}
+    }
+
+	void PerformRotation()
+	{
+		rb.MoveRotation(rb.rotation * Quaternion.Euler (rotation));
+		if (cam != null) {
+			cam.transform.Rotate (-cameraRotation);
+		}
+	}
+}
